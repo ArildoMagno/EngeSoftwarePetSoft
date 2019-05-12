2e98b48b11ed679b53ce53b8ec2c0a8faf6e3f41
Fernando Martinho <martinho672@gmail.com>
2019-05-12 19:40:27 -0300

    CRUD quqase completo fornecedor

---
+++ src/Controle/ControleFornecedor.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controle;
+
+import Modelos.Fornecedor;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author gungnir
+ */
+public class ControleFornecedor {
+
+    public void InserirFornecedor(Fornecedor fornecedor) {
+        Conexao conexao = new Conexao();
+        try {
+            String query = "INSERT INTO Fornecedor  (razaoSocial, CNPJ, nomeFantasia,endereco,telefone)"
+                    + "VALUES(?,?,?,?,?)";
+            PreparedStatement ps = conexao.getConnection().prepareStatement(query);
+            ps.setString(1, fornecedor.getRazaoSocial());
+            ps.setString(2, fornecedor.getCNPJ());
+            ps.setString(3, fornecedor.getNomeFantasia());
+            ps.setString(4, fornecedor.getEndereco());
+            ps.setString(5, fornecedor.getTelefone());
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(ControleFornecedor.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            conexao.closeConnection();
+        }
+    }
+    
+    public ArrayList<Fornecedor> ListaFornecedor(){
+        Conexao conexao = new Conexao();
+        ArrayList<Fornecedor> listaFornecedor = new ArrayList<>();
+        Fornecedor fornecedor = new Fornecedor();
+        try{
+            String query = "SELECT * FROM Fornecedor";
+            Statement st = conexao.getConnection().createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while(rs.next()){
+                fornecedor.setId(rs.getInt("id"));
+                fornecedor.setCNPJ(rs.getString("CNPJ"));
+                fornecedor.setEndereco(rs.getString("endereco"));
+                fornecedor.setNomeFantasia(rs.getString("nomeFantasia"));
+                fornecedor.setRazaoSocial(rs.getString("razaoSocial"));
+                fornecedor.setTelefone(rs.getString("telefone"));
+                listaFornecedor.add(fornecedor);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ControleFornecedor.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            conexao.closeConnection();
+        }
+        return listaFornecedor;
+    }
+}
---
+++ src/Modelos/Fornecedor.java
@@ -0,0 +1,63 @@
+
+package Modelos;
+
+
+public class Fornecedor {
+  private int id;
+  private String nomeFantasia;
+  private String razaoSocial;
+  private String endereco;
+  private String telefone;
+  private String CNPJ;
+
+    public String getCNPJ() {
+        return CNPJ;
+    }
+
+    public void setCNPJ(String CNPJ) {
+        this.CNPJ = CNPJ;
+    }
+  
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+  
+    
+}
